587e20f69ac6cbf4eb0679984223798f4a04839d
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2023-11-26 19:03:44 -0300

    setting up

---
+++ defs.h
@@ -0,0 +1,25 @@
+
+#define TAMMAX 100
+
+typedef struct {
+    union{
+        int attrbI;
+        float attrbF;
+        char attrbC;
+        char attrbS[15];
+    }
+} Column;
+
+typedef struct {
+    unsigned int pk;
+    Column col[TAMMAX];
+} Row;
+
+typedef struct {
+    char name[15];
+    char colname[TAMMAX][15];
+    Row row[TAMMAX];
+} Table;
+
+
+
---
+++ functions/table.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "defs.h"
+
+void createTable(){
+    char tableName[15];
+    //verificar se já existe tabela com esse nome
+
+    //salvar em .txt
+}
+
+void listTables(){
+    char tableName[15];
+}
+
+void listDataFromTable(){
+    char tableName[15];
+
+    //importar do txt
+    //printar
+}
+
+void deleteLine(char tableName[15]){
+    char pk[15];
+    //verificar se a pk existe
+    listDataFromTable()
+    //verificar se é essa msm
+    //apagar linha no txt
+    listDataFromTable()
+}
+
+void dropTable(char tableName[15]){
+    //apagar .txt
+}
+
+void addData(char tableName[15]){
+    
+    listDataFromTable();
+    //verificar se a pk já existe
+    //atualizar txt
+}
+
+void searchData(char tableName[15]){
+    char nameCol[15];
+    char filter[2];
+    //bater o filtro com o tipo de dado da coluna
+}
