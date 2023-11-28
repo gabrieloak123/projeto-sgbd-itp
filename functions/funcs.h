9ad3dec93bf65bebd24675a83426bd7f6ffeba37
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2023-11-28 16:39:19 -0300

    arquivo pra declarar funcs

---
+++ functions/funcs.h
@@ -0,0 +1,15 @@
+#ifndef FUNCS
+#define FUNCS
+
+void printLine(int size);
+void showMenu();
+
+void createTable();
+void listTables();
+void listaDataFromTable();
+void deleteLine();
+void dropTable();
+void addData();
+void searchData();
+
+#endif
