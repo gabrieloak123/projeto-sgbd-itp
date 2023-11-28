f0a5ecb96973f7af354994451c9cf5f18bc98430
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2023-11-28 16:39:31 -0300

    func de print

---
+++ functions/menu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "funcs.h"
+
+void showMenu(){
+    printf("===========================================");
+    printf("Digite o número correspondente à operação:");
+    printf("1. Criar tabela");
+    printf("2. Listar tabelas");
+    printf("3. Adicionar dados");
+    printf("4. Listar dados");
+    printf("5. Pesquisar dados");
+    printf("6. Apagar linha");
+    printf("7. Apagar tabela");
+    printf("8. Sair");
+    printf(===========================================);
+}
