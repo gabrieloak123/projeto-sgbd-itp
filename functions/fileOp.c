f8043378ac7c0cfbbc1b51214e766f810f26d3f4
Dio <vinilimasampaio@gmail.com>
2023-12-12 00:56:36 -0300

    Update fileOp.c
    Aqui está o ponto critical, inicialmente a função estava toda bugada ao separar o tokens e passar as informações para a memória.
    
    Anteriormente '-'...
    * A leitura dos nomes das colunas estavam incluindo vírgula (não queremos isso)
    * Devido a separação em tokens errada também para o tipo das colunas, o programa estava retornando na verificação de erro para tipos inválidos, caso não retornasse era Segmentation Fault
    
    Atualmente =D
    * A passagem de tokens funciona como deveria, agora ele desconsidera espaços em brancos apesar de haver no arquivo (fica mais bonito :P)
    * O programa lê uma linha como um todo do arquivo e separa essa linha nos tokens. Ex: a linha "INT, FLOAT, STRING" é lida, armazenado em string com um tamanho máximo (perigo!) e ai é processado em tokens.

--- functions/fileOp.c
+++ functions/fileOp.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char TableNames[MAX_TABLE_NAME][MAX_NUM_TABLES];
 
@@ -54,27 +55,49 @@ void readTable(Table *table, char *tableName) {
     fscanf(file, "Chave Primária: %d\n", &(table->primaryKeyIndex));
 
     // Leitura do nome das colunas
+    char line[MAX_STRING_SIZE];
+    fgets(line, MAX_STRING_SIZE, file);
+    char *token = strtok(line, ",");
     for (int i = 0; i < table->numColumns; i++) {
-        fscanf(file, "%s", table->columns[i].name);
+        // Remove espaços em branco no início e no final do token
+        token += strspn(token, " ");
+        char *end = token + strlen(token) - 1;
+        while(end > token && isspace((unsigned char)*end)) end--;
+        end[1] = '\0';
+
+        // Copia o nome da coluna para a estrutura da tabela
+        strcpy(table->columns[i].name, token);
+        token = strtok(NULL, ",");
     }
     // Leitura do tipo das colunas
+    fgets(line, MAX_STRING_SIZE, file);
+    token = strtok(line, ",");
     for (int i = 0; i < table->numColumns; i++) {
-        char type[MAX_COLUMN_NAME];
-        fscanf(file, "%s", type);
-        table->columns[i].type = stringToType(type);
+        // Remove espaços em branco no início e no final do token
+        token += strspn(token, " ");
+        char *end = token + strlen(token) - 1;
+        while(end > token && isspace((unsigned char)*end)) end--;
+        end[1] = '\0';
+
+        // Converte a string do tipo de dados para o enum correspondente e armazena na estrutura da tabela
+        table->columns[i].type = stringToType(token);
         if (table->columns[i].type == -1) {
             printf("Tipo de dado não reconhecido na coluna %d.\n", i);
             return;
         }
+        token = strtok(NULL, ",");
     }
 
     // Leitura dos dados com base no modelo atual de colunas
     for (int i = 0; i < table->numRows; i++) {
+        // Lê uma linha do arquivo (Esse MAX_STRING_SIZE deve ser mudado visto que a linha pode ser gigantesca)
         char line[MAX_STRING_SIZE];
         fgets(line, MAX_STRING_SIZE, file);
 
+        // Separa a linha em tokens separados por vírgula e armazena na tabela
         char *token = strtok(line, ",");
         for (int j = 0; j < table->numColumns; j++) {
+            // Converte o token para o tipo de dados da Tabela
             switch (table->columns[j].type) {
                 case INT:
                     table->columns[j].Data.intData[i] = atoi(token);
@@ -89,12 +112,17 @@ void readTable(Table *table, char *tableName) {
                     table->columns[j].Data.charData[i] = token[0];
                     break;
                 case STRING:
+                    // Remove os espaços em branco no início e no final do token
+                    token += strspn(token, " ");
+                    char *end = token + strlen(token) - 1;
+                    while (end > token && isspace((unsigned char) *end)) end--;
+                    end[1] = '\0';
+
+                    // Copia o token para a tabela
                     strcpy(table->columns[j].Data.stringData[i], token);
                     break;
             }
             token = strtok(NULL, ",");
         }
     }
-
-    fclose(file);
 }
